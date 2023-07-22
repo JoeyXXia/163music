9f7bf1f65cc6104ca38bd5202d52fe1a52749ed9
Xu <whitenight.xiaxu@gmail.com>
2023-07-22 20:37:43 +0800

    config craco

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-    <h2>hello</h2>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>hello</h2>
+        </div>
+    );
 }
 
 export default App;
