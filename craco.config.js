e594d61c3fdc924bccee1175d4ab8d7bdf968e18
Xu <whitenight.xiaxu@gmail.com>
2023-07-22 20:52:28 +0800

    config prettier & paths

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,10 @@
-const path = require("path")
-const resolve = dir => path.join(__dirname, dir)
+const path = require('path')
+const resolve = (dir) => path.join(__dirname, dir)
 
 module.exports = {
-    webpack: {
-        alias: {
-            "@": resolve("src")
-        }
-    },
+  webpack: {
+    alias: {
+      '@': resolve('src')
+    }
+  }
 }
