7a51a7c1723e82dfbb374fa705f22ade8435131e
Xu <whitenight.xiaxu@gmail.com>
2023-08-23 21:22:09 +0800

    feat redux type

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,7 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
+import {
+  shallowEqual,
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector
+} from 'react-redux'
 
 const store = configureStore({
   reducer: {}
 })
 
+type GetStateFnType = typeof store.getState
+type IRootState = ReturnType<GetStateFnType>
+type DispathType = typeof store.dispatch
+
+export const useAppSelector: TypedUseSelectorHook<IRootState> = useSelector
+export const useAppDispatch: () => DispathType = useDispatch
+export const shallowEqualApo = shallowEqual
 export default store
