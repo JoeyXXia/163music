37f53252cc29d731e6d61acf3104aade4bd59a64
Xu <whitenight.xiaxu@gmail.com>
2024-07-10 10:41:28 +0800

    delete sprite class

--- src/views/discover/c-views/recommned/c-cpns/user-login/index.tsx
+++ src/views/discover/c-views/recommned/c-cpns/user-login/index.tsx
@@ -6,7 +6,7 @@ interface IProps {
 }
 const UserLogin: FC<IProps> = () => {
   return (
-    <UserLoginWrapper className="sprite_02">
+    <UserLoginWrapper>
       <p className="dec">
         登录网易云音乐，可以享受无限收藏的乐趣， 并且无限同步到手机
       </p>
